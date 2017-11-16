77ca46b84445275ad66136767956f4501c74c03f
Agis Anastasopoulos <agis.anast@gmail.com>
2017-11-16 23:07:37 +0200

    wip

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,41 @@
+SCORES = [3, 4.5, 7]
+MATCHES = {
+  "Don't check" => 0,
+  "At least 1"  => 1,
+  "At least 2"  => 2,
+  "At least 3"  => 3,
+  "All"         => 4,
+}
+
 class SearchController < ApplicationController
   def index
   end
 
   def show
     begin
-      @hex = Chroma.paint(params[:hex]).to_hex
+      @bg_color = Chroma.paint(URI.decode(params[:bg_color])).to_hex
     rescue Chroma::Errors::UnrecognizedColor
       redirect_to root_path, alert: "Wrong format"
       return
     end
 
+    @page = (params[:page] || 0).to_i
+
+    # filters
+    @score = (params[:score] || SCORES[0]).to_f
+    @matches = (params[:matches] || 0).to_i
+
+    qs = {
+      format: "json",
+      orderCol: "score",
+      sortBy: "DESC",
+      hex: @bg_color[1..-1],
+      numResults: 50,
+      resultOffset: @page
+    }.to_query
+
     @palettes = JSON.parse(Faraday.get(
-      "http://www.colourlovers.com/api/palettes?format=json&hex=#{@hex[1..-1]}"
+      "http://www.colourlovers.com/api/palettes?#{qs}"
     ).body)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,22 @@
 module ApplicationHelper
-  def ratio(hex, color)
-    number_with_precision(WCAGColorContrast.ratio(hex.sub("#",""), color), precision: 2)
+  # @param a [String] first color
+  # @param b [String] second color
+  #
+  # @return [Float]
+  def ratio(a, b)
+    WCAGColorContrast.ratio(a.sub("#",""), b).round(2)
+  end
+
+  # @return [Boolean]
+  def passes?(palette, bg_color, score, matches)
+    current_matches = 0
+
+    palette["colors"].reject { |c| c == bg_color[1..-1].upcase }.each do |color|
+      color_score = ratio(bg_color, color)
+      current_matches += 1 if color_score >= score
+      return true if current_matches >= matches
+    end
+
+    false
   end
 end
